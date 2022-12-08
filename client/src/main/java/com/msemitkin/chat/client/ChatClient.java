2c400e96bcfe45c584963e468126924f4139830e
Mykhailo Semitkin <michael.semitkin@gmail.com>
2022-12-08 23:55:45 +0200

    implemented two-way communication between client and server

--- client/src/main/java/com/msemitkin/chat/client/ChatClient.java
+++ client/src/main/java/com/msemitkin/chat/client/ChatClient.java
@@ -1,29 +1,56 @@
 package com.msemitkin.chat.client;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.UncheckedIOException;
 import java.net.Socket;
 
 public class ChatClient {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+        new ChatClient().start();
+    }
+
+    void start() throws IOException {
+        Socket server = new Socket("localhost", 6666);
+        System.out.println("Successfully connected to server");
+        new Thread(() -> sendUpdatesToServer(server)).start();
+        new Thread(() -> listenToUpdatesFromServer(server)).start();
+    }
+
+    private void listenToUpdatesFromServer(Socket serverSocket) {
         try (
-            Socket server = new Socket("localhost", 6666);
+            var serverInputStreamReader = new InputStreamReader(serverSocket.getInputStream());
+            BufferedReader serverReader = new BufferedReader(serverInputStreamReader)
+        ) {
+            System.out.println("Waiting for updates from the server");
+            String line;
+            while ((line = serverReader.readLine()) != null) {
+                System.out.println("From server: " + line);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void sendUpdatesToServer(Socket server) {
+        try (
             InputStreamReader inputStreamReader = new InputStreamReader(System.in);
             BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
-
             OutputStream outputStream = server.getOutputStream();
             PrintWriter printWriter = new PrintWriter(outputStream, true)
         ) {
-            System.out.println("Successfully connected to server");
-            while (true) {
+            System.out.println("Waiting for updates from user");
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
                 System.out.println("Waiting for input");
-                String input = bufferedReader.readLine();
-                printWriter.println(input);
+                printWriter.println(line);
             }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
         }
     }
 }
--- server/src/main/java/com/msemitkin/chat/server/ChatServer.java
+++ server/src/main/java/com/msemitkin/chat/server/ChatServer.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,8 +19,8 @@ public class ChatServer {
             System.out.printf("Started server on %d port%n", port);
             while (true) {
                 Socket clientSocket = serverSocket.accept();
-                System.out.println("New client: " + clientSocket.getPort());
                 new Thread(() -> listenToClient(clientSocket)).start();
+                System.out.println("New client: " + clientSocket.getPort());
             }
         }
     }
@@ -28,11 +29,13 @@ public class ChatServer {
         try (
             InputStream socketInputStream = socket.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(socketInputStream);
-            BufferedReader socketReader = new BufferedReader(inputStreamReader)
+            BufferedReader socketReader = new BufferedReader(inputStreamReader);
+            PrintWriter printWriter = new PrintWriter(socket.getOutputStream(), true);
         ) {
             String line;
             while ((line = socketReader.readLine()) != null) {
-                System.out.println("Received: ".concat(line));
+                System.out.printf("Received from %s: %n".concat(line), socket.getPort());
+                printWriter.println("Received from you: " + line);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
